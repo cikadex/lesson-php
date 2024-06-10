63bed8ac833f305c17c10ddebcfedbfa15154847
cikadex <cikadex@yandex.ru>
2024-06-10 20:05:23 +0700

    2024.1

---
+++ lesson9/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Урок 9. Арифметически операторы</title>
+</head>
+
+<body>
+
+</body>
+</html>
